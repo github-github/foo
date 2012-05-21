7c79e39c2d7160bc1838323638a1da70fd15f0fc
foobar <foobar@foobar.com>
2012-05-21 19:45:45 +0200

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HelloWorld
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, world.");
+    }
+  }
+}
